38640d8e86ce7f8276c13288c98fb599189bf308
PRIMARY\Ryan <ryan.jack.taylor@gmail.com>
2018-05-09 18:30:33 -0500

    Test 3 now passes.

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -21,7 +21,13 @@ public class VendingMachine {
             @Override
             public void onCoinReceived(int coinValue) {
                 sUserCreditInCents+=coinValue;
-                mDisplay = "$" + String.valueOf(sUserCreditInCents/100) + "." + String.valueOf(sUserCreditInCents%100);
+
+                //If there is a value, display it.  If it's 0, show INSERT COIN
+                if (sUserCreditInCents > 0) {
+                    mDisplay = "$" + String.valueOf(sUserCreditInCents / 100) + "." + String.valueOf(sUserCreditInCents % 100);
+                } else {
+                    mDisplay = INSERT_COIN_TEXT;
+                }
             }
         });
     }
